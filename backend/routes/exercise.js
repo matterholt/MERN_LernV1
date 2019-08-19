26aaad6b7462ffe4c7073b4779cab501189d2dde
MAtterholt <matterholt84@gmail.com>
2019-08-18 20:10:54 -0400

    create exercise routes

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -2,8 +2,30 @@ const router = require("express").Router();
 let Exercise = require("../models/exercise.model");
 
 // route to get exercises
-router.runInContext("/").get((req, res) => {
+router.route("/").get((req, res) => {
   Exercise.find()
     .then(exercises => res.JSON(exercise))
     .catch(err => res.status(400).json("Error: " + err));
 });
+
+//
+router.route("/add").post((req, res) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const data = Date.parse(req.body.date);
+
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    data
+  });
+
+  newExercise.save();
+  then(() => res.json("Exercise added!")).catch(err =>
+    res.status(400).json("Error: " + err)
+  );
+});
+
+module.exports = router;
