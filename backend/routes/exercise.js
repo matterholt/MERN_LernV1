1f8f6a0a51708d3a926394c014ae5621178a02b4
MAtterholt <matterholt84@gmail.com>
2019-08-18 22:41:00 -0400

    route for update excercise

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -33,9 +33,26 @@ router.route("/:id").get((req, res) => {
     .then(exercise => res.json(exercise))
     .catch(err => res.status(400).json("Error: " + err));
 });
+// DELETE exercise
 router.route("/:id").delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
     .then(() => res.json("Exercise deleted!"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+// UPDATE EXERCISE
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.param.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.data = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise update!"))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
     .catch(err => res.status(400).json("Error: " + err));
 });
 
