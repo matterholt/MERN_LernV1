b7db41a0be5ec7f3a0fde5a8e3dcbf3926877932
MAtterholt <matterholt84@gmail.com>
2019-08-19 22:32:02 -0400

    set up react route, and placeholder for componets

--- src/App.js
+++ src/App.js
@@ -3,26 +3,23 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./component/navbar.component";
-/*
-import ExerciseList from "./component/exercise-list.component";
-import EditExercise from "./component/edit-exercise.component";
-import CreateExercise from "./component/create-exercise.component";
-import CreateUser from "./component/create-user.component";
-*/
+import ExerciseList from "./component/exerciseList.component";
+import EditExercise from "./component/editExercise.component";
+import CreateExercise from "./component/createExercise.component";
+import CreateUser from "./component/createUser.component";
 function App() {
   return (
     <Router>
-      <NavBar />
-      <br />
+      <div className="container">
+        <NavBar />
+        <br />
+        <Route path="/" exact component={ExerciseList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
-
-/*
-      <Route path="/" exact component={ExerciseList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
-*/
 
 export default App;
