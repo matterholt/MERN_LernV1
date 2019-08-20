a2855b0234c7e29c7352128ef72d45d8f6bdce41
MAtterholt <matterholt84@gmail.com>
2019-08-19 22:17:23 -0400

    create navbar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import NavBar from "./component/navbar.component";
+/*
 import ExerciseList from "./component/exercise-list.component";
 import EditExercise from "./component/edit-exercise.component";
 import CreateExercise from "./component/create-exercise.component";
 import CreateUser from "./component/create-user.component";
-
+*/
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavBar />
       <br />
+    </Router>
+  );
+}
+
+/*
       <Route path="/" exact component={ExerciseList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
       <Route path="/user" component={CreateUser} />
-    </Router>
-  );
-}
+*/
 
 export default App;
