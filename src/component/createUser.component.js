8e58be8d81f10fcd259dfc2ff37cfb7bc1f4784e
MAtterholt <matterholt84@gmail.com>
2019-08-22 19:10:04 -0400

    build user page with logic

--- src/component/createUser.component.js
+++ src/component/createUser.component.js
@@ -1,10 +1,47 @@
 import React, { Component } from "react";
 
 export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: ""
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const username = { user: this.state.username };
+    console.log(username);
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the Create User component!</p>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username:</label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={e => {
+                this.setState({ username: e.target.value });
+              }}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Create User"
+            />
+          </div>
+        </form>
       </div>
     );
   }
